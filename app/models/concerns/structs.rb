1816e157941768dc9f3940119982c20dddc34167
Saad Azam <zeroinside4u@gmail.com>
2025-05-31 13:33:00 +0500

    rdoc

--- app/models/concerns/structs.rb
+++ app/models/concerns/structs.rb
@@ -1,7 +1,9 @@
 # Collection of Structs beings used throughout the application
 module Structs
+  # Struct to store details about details of Machine Learning models
   DetailStruct = Data.define(:title, :description)
 
+  # Struct to store model, details, parameters for making production and its identifier (id)
   MlModelStruct = Data.define(:model, :details, :prediction_params, :id) do
     delegate :predict, to: :model
 
