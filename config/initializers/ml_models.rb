15943a4bc153a32b4733ce0187ccefa498e6016d
Saad Azam <zeroinside4u@gmail.com>
2025-06-06 02:04:16 +0500

    Temp commit

--- config/initializers/ml_models.rb
+++ config/initializers/ml_models.rb
@@ -1,67 +1,67 @@
-Rails.application.config.after_initialize do
-  ML_MODELS = {}
+# Rails.application.config.after_initialize do
+#   ML_MODELS = {}
 
-  mnist_model_path = Rails.root.join("lib", "models", "mnist.pth").to_s
-  wine_quality_model_path = Rails.root.join("lib", "models", "random_forest_regressor.dat").to_s
+#   mnist_model_path = Rails.root.join("lib", "models", "mnist.pth").to_s
+#   wine_quality_model_path = Rails.root.join("lib", "models", "random_forest_regressor.dat").to_s
 
-  ML_MODELS[:mnist] = Structs.model(
-    Torch::Models::MnistModel.load_dict(mnist_model_path),
-    Structs.details("MNIST", "Cool MNIST Model"),
-    { input_image: { type: :image, label: "Input Image" } },
-    :mnist
-  ) if File.exist?(mnist_model_path)
+#   ML_MODELS[:mnist] = Structs.model(
+#     Torch::Models::MnistModel.load_dict(mnist_model_path),
+#     Structs.details("MNIST", "Cool MNIST Model"),
+#     { input_image: { type: :image, label: "Input Image" } },
+#     :mnist
+#   ) if File.exist?(mnist_model_path)
 
-  ML_MODELS[:wine_quality] = Structs.model(
-    Rumale::Models::WineQualityModel.load_saved_model,
-    Structs.details("Wine Quality", "Check Wine Quality"),
-    {
-      fixed_acidity: {
-        type: :number,
-        label: "Fixed Acidity"
-      },
-      volatile_acidity: {
-        type: :number,
-        label: "Volatile Acidity"
-      },
-      citric_acid: {
-        type: :number,
-        label: "Citric Acid"
-      },
-      residual_sugar: {
-        type: :number,
-        label: "Residual Sugar"
-      },
-      chlorides: {
-        type: :number,
-        label: "Chlorides"
-      },
-      free_sulfur_dioxide: {
-        type: :number,
-        label: "Free Sulfur Dioxide"
-      },
-      total_sulfur_dioxide: {
-        type: :number,
-        label: "Total Sulfur Dioxide"
-      },
-      density: {
-        type: :number,
-        label: "Density"
-      },
-      ph: {
-        type: :number,
-        label: "pH"
-      },
-      sulphates: {
-        type: :number,
-        label: "Sulphates"
-      },
-      alcohol: {
-        type: :number,
-        label: "Alcohol"
-      }
-    },
-    :wine_quality
-  )
+#   ML_MODELS[:wine_quality] = Structs.model(
+#     Rumale::Models::WineQualityModel.load_saved_model,
+#     Structs.details("Wine Quality", "Check Wine Quality"),
+#     {
+#       fixed_acidity: {
+#         type: :number,
+#         label: "Fixed Acidity"
+#       },
+#       volatile_acidity: {
+#         type: :number,
+#         label: "Volatile Acidity"
+#       },
+#       citric_acid: {
+#         type: :number,
+#         label: "Citric Acid"
+#       },
+#       residual_sugar: {
+#         type: :number,
+#         label: "Residual Sugar"
+#       },
+#       chlorides: {
+#         type: :number,
+#         label: "Chlorides"
+#       },
+#       free_sulfur_dioxide: {
+#         type: :number,
+#         label: "Free Sulfur Dioxide"
+#       },
+#       total_sulfur_dioxide: {
+#         type: :number,
+#         label: "Total Sulfur Dioxide"
+#       },
+#       density: {
+#         type: :number,
+#         label: "Density"
+#       },
+#       ph: {
+#         type: :number,
+#         label: "pH"
+#       },
+#       sulphates: {
+#         type: :number,
+#         label: "Sulphates"
+#       },
+#       alcohol: {
+#         type: :number,
+#         label: "Alcohol"
+#       }
+#     },
+#     :wine_quality
+#   )
 
-  ML_MODELS.freeze
-end
+#   ML_MODELS.freeze
+# end
---
+++ config/initializers/pycall.rb
@@ -0,0 +1,4 @@
+Rails.application.config.after_initialize do
+  site = PyCall.import_module('site')
+  site.addsitedir(ENV['POETRY_PACKAGES'])
+end
