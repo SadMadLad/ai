3e53aa33647ea23067a7ff50f8901f2eb449e9e8
Saad Azam <zeroinside4u@gmail.com>
2025-06-14 18:26:08 +0500

    Parallel

--- app/models/embedding.rb
+++ app/models/embedding.rb
@@ -4,4 +4,10 @@ class Embedding < ApplicationRecord
   validates :embedding_model, presence: true, uniqueness: { scope: %i[ embeddable_id embeddable_type ] }
 
   enum :embedding_model, OllamaClient.embedding_models
+
+  class << self
+    Embedding.embedding_models.keys.each do |embedding_model|
+      define_method(embedding_model) { Embedding.where(embedding_model:) }
+    end
+  end
 end
--- app/services/embedding_service.rb
+++ app/services/embedding_service.rb
@@ -11,12 +11,14 @@ class EmbeddingService < ApplicationService
 
   private
     def process_records
-      all_embeddings = @record.map do |record|
-        @embedding_models.each_slice(2).map do |embedding_models|
-          embedding_data = Parallel.map(embedding_models, in_threads: embedding_models.length) do |embedding_model|
-            embedding = @client.embed(model: OllamaClient.models[embedding_model], text: record[@embeddable_column]).first
+      all_embeddings = @record.each_slice(2).map do |records|
+        Parallel.map(records, in_threads: records.length) do |record|
+          @embedding_models.each_slice(2).map do |embedding_models|
+            embedding_data = Parallel.map(embedding_models, in_threads: embedding_models.length) do |embedding_model|
+              embedding = @client.embed(model: OllamaClient.models[embedding_model], text: record[@embeddable_column]).first
 
-            { embeddable: record, embedding:, embedding_model: }
+              { embeddable: record, embedding:, embedding_model: }
+            end
           end
         end
       end
