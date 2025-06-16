b79472da676aaa6d8bdd1685af5eb695f8e5a094
Saad Azam <zeroinside4u@gmail.com>
2025-06-17 04:34:12 +0500

    Serializer

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Ai
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.autoload_once_paths << "#{root}/app/serializers"
   end
 end
